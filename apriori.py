685a7b44e4e4e638a344d700a8ad7b1ca2ef3d7c
ktagg <kyle.taggard96@gmail.com>
2018-09-28 11:39:45 -0700

    Commiting work Andrew did.

--- apriori.py
+++ apriori.py
@@ -1,5 +1,50 @@
 import sys
+import csv
 
+goodsInfoDictionary = dict()
+goodsCountDictionary = dict()
+
+class Goods:
+    def __init__(self,flavor,food,price,type):
+        self.flavor = flavor
+        self.food = food
+        self.price = price
+        self.type = type
+        
+def parseReceipts():
+    for x in range(50):
+        goodsCountDictionary[x] = 0
+        
+    with open('1000/1000-out1.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        rowCount = 0
+        for row in csv_reader:
+            print "Receipt"+ row[0]
+            for s in row[1:]:
+                print int(s.strip())
+                goodsCountDictionary[int(s.strip())] += 1
+                #if(goodsCountDictionary.has_key(int(s.strip()))):
+                 #   goodsCountDictionary[s] += 1                    
+            rowCount += 1 
+    for i in goodsCountDictionary:
+        print i, goodsCountDictionary[i] 
+                  
+def parseGoods():
+    with open('goods.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        lineCount = 0
+        for row in csv_reader:
+            if (lineCount > 0):
+                print "ID"+ row[0]
+                marketGood = Goods(row[1], row[2],row[3],row[4])
+                goodsID = lineCount - 1
+                
+                goodsInfoDictionary[goodsID] = marketGood
+                lineCount += 1
+            else:
+                lineCount += 1
+                                               
+                       
 def apriori(T, I, minSup):
     F = 1
     k = 2
@@ -13,3 +58,6 @@ def apriori(T, I, minSup):
                     count[c] + 1
         k = k + 1
     return F
+
+#parseGoods()
+parseReceipts()
